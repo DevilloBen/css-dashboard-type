7fa88ffbd5d44745f59e24b0257b551be1cdbe72
DevilloBen <sakhonkam.b@gmail.com>
2021-08-02 00:10:22 +0700

    version 1 css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sidebar">
+      <div className="logo-content">
+        <div className="logo">
+          <i className='bx bxl-aws'></i>
+          <div className="logo-name"> Devillo</div>
+        </div>
+        <i className='bx bx-menu' id="btn"></i>
+      </div>
+      <ul className="nav-list">
+      <li>
+            <i className="bx bx-search-alt"></i>
+            <input type="text" placeholder="search" className=""></input>
+          
+        </li>
+        <li>
+          <a href="/#">
+            <i className="bx bxs-dashboard"></i>
+            <span className="links-name">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <i className="bx bx-home-alt"></i> 
+            <span className="links-name">Home</span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <i className="bx bx-user"></i>
+            <span className="links-name">User</span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <i className="bx bx-line-chart"></i>
+            <span className="links-name">Analysis</span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <i className="bx bx-cog"></i> 
+            <span className="links-name">Setting</span>
+          </a>
+        </li>
+      </ul>
+      <div className="profile-content">
+        <div className="profile">
+          <div className="profile-details">
+            <img src="./1.jpg" alt=""/>
+            <div className = "name-job">
+              <div className="name">Benchapon Sakhonkam</div>
+              <div className="job">Front-End Engineer</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul className="nav-list">
+      <li>
+      <a href="/#">
+      <i className="bx bx-log-out" id ="log-out"></i>
+          <span className="links-name"> Log-out</span>
+      </a>
+      </li>
+      </ul>
+     
     </div>
   );
 }
